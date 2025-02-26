e753a886cbf7bfb0c3055fb23bcb16f70323c130
lucy2512 <mail4parthib@gmail.com>
2025-02-26 10:43:08 +0530

    redux added

--- react-frontend/src/redux/features/authSlice.js
+++ react-frontend/src/redux/features/authSlice.js
@@ -1,50 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    user: null,
-    accessToken: null,
-    refreshToken: null,
-    loading: false,
-    error: null
+    accessToken: localStorage.getItem("accessToken") || null,
+    refreshToken: localStorage.getItem("refreshToken") || null,
+    isAuthenticated: false
 }
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        loginStart: (action, state) => {
-            state.loading = true
-        },
-
-        loginSuccess: (action, state) => {
+        login: (state, action) => {
             state.user = action.payload.user;
             state.accessToken = action.payload.accessToken;
             state.refreshToken = action.payload.refreshToken;
-            state.loading = false;
+            state.isAuthenticated = true;
+
+            // //set Tokens in the localStorage
+            localStorage.setItem("accessToken", action.payload.accessToken);
+            localStorage.setItem("refreshToken", action.payload.refreshToken);
         },
 
-        loginFail: (action, state) => {
-            state.loading = false;
-            state.error = action.payload.error;
-        },
-
-        logout: (action, state) => {
+        // eslint-disable-next-line no-unused-vars
+        logout: (state, action) => {
             state.user = null;
             state.accessToken = null;
-            state.refreshToken = null
+            state.refreshToken = null;
+            state.isAuthenticated = false;
+
+            //remove Tokens from localStorage
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("refreshToken");
         },
 
-        setLoading: (action, state) => {
-            state.loading = action.payload;
-        },
+        //setting tokens again into the state after backend (/refresh-token) is getting called
 
-        setError: (action, state) => {
-            state.error = action.payload;
-        }
+        setTokens: (state, action) => {
+            state.accessToken = action.payload.accessToken;
+            state.refreshToken = action.payload.refreshToken;
+            state.isAuthenticated = true;
+
+            localStorage.clear();
+            localStorage.setItem("accessToken", action.payload.accessToken);
+            localStorage.setItem("refreshToken", action.payload.refreshToken);
+        },
 
     }
 });
 
-export const { loginStart, loginSuccess, loginFail, logout, setLoading, setError } = authSlice.actions;
 
+//this export is for individual ,ethod export for componenets
+export const { login, logout, setTokens } = authSlice.actions;
+
+// this export is all reducers export for the store.js
 export default authSlice.reducer;
--- react-frontend/src/services/axiosInstance.js
+++ react-frontend/src/services/axiosInstance.js
@@ -1,82 +1,94 @@
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setTokens } from "../redux/features/authSlice"
 
 
-const API_URL = import.meta.env.VITE_API_URL;
+const useAxios = () => {
+    const API_URL = import.meta.env.VITE_API_URL;
+    const dispatch = useDispatch();
 
-const axiosInstance = axios.create({
-    baseURL: API_URL,
-    headers: { "Content-Type": "application/json" }
-})
+    const axiosInstance = axios.create({
+        baseURL: API_URL,
+        headers: { "Content-Type": "application/json" }
+    })
 
-axiosInstance.interceptors.request.use(
-    (config) => {
-        console.log(config);
-        const token = localStorage.getItem("accessToken");
-        console.log(token);
 
-        if (token) {
-            config.headers["Authorization"] = `Bearer ${token}`;
-            console.log("Token set to the header");
+
+    axiosInstance.interceptors.request.use(
+        (config) => {
             console.log(config);
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
-)
+            const token = localStorage.getItem("accessToken");
+            // console.log(token);
 
-axiosInstance.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        const originalRequest = error.config;
+            if (token) {
+                config.headers["Authorization"] = `Bearer ${token}`;
+                console.log("Token set to the header");
+                console.log(config);
+            }
+            return config;
+        },
+        (error) => Promise.reject(error)
+    )
 
-        //If AccessToken expires then refreshing the token
+    axiosInstance.interceptors.response.use(
+        (response) => response,
+        async (error) => {
+            const originalRequest = error.config;
+
+            //If AccessToken expires then refreshing the token
 
 
-        if (error.response?.status === 401 && !originalRequest._retry) {
-            originalRequest._retry = true;
+            if (error.response?.status === 401 && !originalRequest._retry) {
+                originalRequest._retry = true;
 
-            try {
-                console.warn("Access Token expired. Trying to refresh token........");
-                const frontendRefreshToken = localStorage.getItem("refreshToken");
-                // console.log(frontendRefreshToken);
-                if (!frontendRefreshToken) {
-                    console.error("NO refersh token found.......");
+                try {
+                    console.warn("Access Token expired. Trying to refresh token........");
+                    const frontendRefreshToken = localStorage.getItem("refreshToken");
+                    // console.log(frontendRefreshToken);
+                    if (!frontendRefreshToken) {
+                        console.error("NO refersh token found.......");
+                        localStorage.clear();
+                        return Promise.reject(error);
+                    }
+
+                    const { data } = await axiosInstance.post("/refresh-token", {
+                        refreshToken: frontendRefreshToken,
+                    });
+                    // console.log("HERE");
+                    // console.log("data:", data);
+                    const tokens = data.tokens;
+                    console.log("Tokens frontend", tokens);
+                    dispatch(setTokens(tokens));
+                    // localStorage.clear()
+                    // localStorage.setItem("accessToken", data.tokens.accessToken);
+                    // localStorage.setItem("refreshToken", data.tokens.refreshToken);
+
+                    return axiosInstance(originalRequest);
+                } catch (err) {
+                    console.error("Refresh token invalid.", err);
                     localStorage.clear();
-                    return Promise.reject(error);
+                    window.href.location = '/login'
                 }
+            }
 
-                const { data } = await axiosInstance.post("/refresh-token", {
-                    refreshToken: frontendRefreshToken,
-                });
-                console.log("HERE");
-                console.log("data:", data);
-                localStorage.clear()
-                localStorage.setItem("accessToken", data.tokens.accessToken);
-                localStorage.setItem("refreshToken", data.tokens.refreshToken);
 
-                return axiosInstance(originalRequest);
-            } catch (err) {
-                console.error("Refresh token invalid.", err);
+            // If user is unauthorized
+
+            if (error.response && error.response.status === 403) {
+                console.error("(React) Unauthorized! Redirecting to login");
                 localStorage.clear();
-                window.href.location = '/login'
+                // window.location.href = "/login";
+            }
+
+            if (error.response && error.response.status === 500) {
+                console.error("Something went wrong", error);
+                // slocalStorage.clear();
+                // window.location.href = "/login";
             }
         }
+    );
 
+    return axiosInstance;
+};
 
-        // If user is unauthorized
-
-        if (error.response && error.response.status === 403) {
-            console.error("(React) Unauthorized! Redirecting to login");
-            localStorage.clear();
-            // window.location.href = "/login";
-        }
-
-        if (error.response && error.response.status === 500) {
-            console.error("Something went wrong", error);
-            // slocalStorage.clear();
-            // window.location.href = "/login";
-        }
-    }
-)
-
-export default axiosInstance;
+export default useAxios;
